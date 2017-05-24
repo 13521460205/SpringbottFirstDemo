7d040bea4c7d7fde029c051c40a333f0b4b5e9ee
huangmao <37390402@qq.com>
2017-05-24 16:26:50 +0800

    修改启动步骤

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.print("第一个spring boot 程序");
 	}
 }
